177c250d7f3094672a9e7a0843eede2cd8a51b5c
ColdsteelRail <574252631@qq.com>
2020-08-17 00:00:01 +0800

    countDownLatch_test

---
+++ build/release-cpp11/muduo/base/test/CMakeFiles/countDownLatch_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/countDownLatch_test.dir/CountDownLatch_test.cc.o"
+  "../../../bin/countDownLatch_test.pdb"
+  "../../../bin/countDownLatch_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/countDownLatch_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
