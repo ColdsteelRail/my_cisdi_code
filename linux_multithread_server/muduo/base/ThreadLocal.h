92c49ba927a3f0744c8ebc94182843ca46c0a974
ColdsteelRail <574252631@qq.com>
2020-08-17 15:42:20 +0800

    threadLocal{Singleton}{_test.cc .h .cc}

---
+++ linux_multithread_server/muduo/base/ThreadLocal.h
@@ -0,0 +1,65 @@
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
+//
+// Author: Shuo Chen (chenshuo at chenshuo dot com)
+
+#ifndef MUDUO_BASE_THREADLOCAL_H
+#define MUDUO_BASE_THREADLOCAL_H
+
+#include "muduo/base/Mutex.h"
+#include "muduo/base/noncopyable.h"
+
+#include <pthread.h>
+
+namespace muduo
+{
+
+template<typename T>
+class ThreadLocal : noncopyable
+{
+
+    public:
+        ThreadLocal()
+        {
+            // 回调函数destrucot来删除实际数据T
+            MCHECK(pthread_key_create(&pkey_, &ThreadLocal::destructor));
+        }
+
+        ~ThreadLocal()
+        {
+            // 仅仅销毁kye,并没有销毁实际数据
+            MCHECK(pthread_key_delete(pkey_));
+        }
+
+        T& value()
+        {
+            // get获取 set设置
+            T* perThreadValue = static_cast<T*>(pthread_getspecific(pkey_));
+            if (!perThreadValue)
+            {
+                T* newObj = new T();
+                MCHECK(pthread_setspacific(pkey_, newObj));
+                perThreadValue = newObj;
+            }
+            return *perThreadValue;
+        }
+
+    private:
+        static void destructor()
+        {
+            T* obj = static_cast<T*>(x);
+            typedef char T_must_be_complete_type[sizeof(T) == 0 ? -1 : 1];
+            T_must_be_complete_type dummy; (void) dummy;
+            delete obj;
+        }
+
+    private:
+        // 线程特定数据
+        // pthread_key_t 使用后，每个线程都将被动拥有，但存储数据是私有的
+        // pthread_key_t 中的数据是堆栈式的
+        pthread_key_t pkey_;
+};
+
+} // namespace muduo
+
+#endif
---
+++ linux_multithread_server/muduo/base/ThreadLocalSingleton.h
@@ -0,0 +1,79 @@
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
+//
+// Author: Shuo Chen (chenshuo at chenshuo dot com)
+
+#ifndef MUDUO_BASE_THREADLOCALSINGLETON_H
+#define MUDUO_BASE_THREADLOCALSINGLETON_H
+
+#include "muduo/base/noncopyable.h"
+
+#include <assert.h>
+#include <pthread.h>
+
+namespace muduo
+{
+
+template<typename T>
+class ThreadLocalSingleton : noncopyable
+{
+    public:
+        ThreadLocalSingleton() = delete;
+        ~ThreadLocalSingleton() = delete;
+    
+
+    
+    private:
+        static void destructor(void* obj)
+        {
+            assert(obj == t_value_);
+            typedef char T_must_be_complete_type[sizeof(T) == 0 ? -1 : 1];
+            T_must_be_complete_type dummy; (void) dummy;
+            delete t_value_;
+            t_value_ = 0;
+        }
+
+        static T& instance()
+        {
+            if (!t_value_)
+            {
+                t_value_ = new T();
+                deleter_.set(t_value_);
+            }
+            return *t_value_;
+        }
+
+        static T* pointer()
+        {
+            return t_value_;
+        }
+
+        class Deleter
+        {
+        public:
+            Deleter()
+            {
+                pthread_key_create(&pkey_, &ThreadLocalSingleton::destructor);
+            }
+
+            ~Deleter()
+            {
+                pthread_key_delete(pkey_);
+            }
+
+            void set(T* newObj)
+            {
+                assert(pthread_getspecific(pkey_) == NULL);
+                pthread_setspecific(pkey_, newObj);
+            }
+
+            pthread_key_t pkey_;
+        };
+
+        static __thread T* t_value_; // POD类型数据，线程独有
+        static Deleter deleter_;
+};
+
+} // namespace muduo
+
+#endif  // MUDUO_BASE_THREADLOCALSINGLETON_H
