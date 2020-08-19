41bedf4c63993d5f6bff10875f8bbf20ea98fa8b
ColdsteelRail <574252631@qq.com>
2020-08-19 21:19:47 +0800

    delete build/

--- build/release-cpp11/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dcn-space/myworkspace/my_cisdi_code/linux_multithread_server
-# Build directory: /home/dcn-space/myworkspace/my_cisdi_code/build/release-cpp11
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(muduo/base)
--- build/release-cpp11/muduo/base/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dcn-space/myworkspace/my_cisdi_code/linux_multithread_server/muduo/base/test
-# Build directory: /home/dcn-space/myworkspace/my_cisdi_code/build/release-cpp11/muduo/base/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(atomic_unittest "/home/dcn-space/myworkspace/my_cisdi_code/build/release-cpp11/bin/atomic_unittest")
-SUBDIRS(testlog)
