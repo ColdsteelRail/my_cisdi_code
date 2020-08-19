890635733faf45930a88198f29bd8a7b73160e5c
ColdsteelRail <574252631@qq.com>
2020-08-19 10:58:07 +0800

    testlog

---
+++ linux_multithread_server/muduo/base/test/testlog/Log_test1.cc
@@ -0,0 +1,16 @@
+#include <muduo/base/Logging.h>
+#include <errno.h>
+
+int main()
+{
+    LOG_TRACE << "trace ...";
+    LOG_DEBUG << "debug ...";
+    LOG_INFO << "info ...";
+    LOG_WARN << "warn ...";
+    LOG_ERROR << "error ...";
+    // LOG_FATAL << "fetal ...";
+    errno = 13;
+    LOG_SYSERR << "syserr ...";
+    LOG_SYSFATAL << "sysfetal ...";
+    return 0;
+}
---
+++ linux_multithread_server/muduo/base/test/testlog/Log_test2.cc
@@ -0,0 +1,39 @@
+#include <muduo/base/Logging.h>
+#include <errno.h>
+#include <stdio.h>
+
+FILE* g_file;
+
+void dummyOutput(const char* msg, int len)
+{
+    if(g_gile)
+    {
+        fwrite(msg, 1, len, gfile);
+    }
+}
+
+void dummyFlush()
+{
+    fflush(g_file);
+}
+
+int main()
+{
+    g_file = ::fopen("temp/muduo_log","ae");
+    muduo::Logger::setOutput(dummyOutput);
+    muduo::Logger::setFlush(dummyFlush);
+
+    LOG_TRACE<<"trace ...";
+    LOG_DEBUG<<"debug ...";
+    LOG_INFO<<"info ...";
+    LOG_WARN<<"warn ...";
+    LOG_ERROR<<"error ...";
+    //LOG_FATAL<<"fatal ...";
+    errno = 13;
+    LOG_SYSERR<<"syserr ...";
+    //LOG_SYSFATAL<<"sysfatal ...";
+
+    :fclose(g_file);
+
+    return 0;
+}
